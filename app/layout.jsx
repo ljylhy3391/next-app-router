2effdeff84c2c5daa70adb1af091cec579e5040f
Leeseongmin <ljylhy3391@naver.com>
2025-11-26 22:13:44 +0900

    refactor: layout.jsx 메타데이터 설정 간소화

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,28 +3,8 @@ import "./globals.css";
 import AppNavigation from "./components/layout/AppNavigation";
 
 export const metadata = {
-  metadataBase: new URL(
-    process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
-  ),
   title: "Next X AI",
   description: "한땀 한땀 치는 시대는 끝?",
-  icons: {
-    icon: "/favicon.ico",
-  },
-  openGraph: {
-    type: "website",
-    title: "Next X AI",
-    description: "한땀 한땀 치는 시대는 끝?",
-    siteName: "Next X AI",
-    images: [
-      {
-        url: "/opengraph-image.png",
-        width: 1200,
-        height: 630,
-        alt: "Next X AI",
-      },
-    ],
-  },
 };
 
 export default function RootLayout({ children }) {
